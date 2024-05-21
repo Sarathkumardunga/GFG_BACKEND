bc17d61ec9f0a5b317dc9bc84d5f3becf6c9922c
Sarath Kumar Dunga <sdunga1@asu.edu>
2024-05-21 16:06:33 -0700

    remove .env file from git

--- TODO_APP/db.js
+++ TODO_APP/db.js
@@ -1,1 +1,8 @@
 //File - To keep database setup part
+
+const mongoose = require('mongoose');
+require('dotenv').config(); //we want to use main mongo url
+
+
+//To connect the Mongo db from the database using the variable
+mongoose.connect(process.env.MONGO_URL);
