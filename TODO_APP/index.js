533d2d0f090c8e053936f8f3fe0ab8fd63ea387b
Sarath Kumar Dunga <sdunga1@asu.edu>
2024-05-21 15:43:15 -0700

    Set up the local server

--- TODO_APP/index.js
+++ TODO_APP/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 8000;
+const PORT = 8000;  //27017
 
+//This line helps to identify the .env file and access it
+//We can treat the variable in it as a global variable and use it.
+require('dotenv').config(); 
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API is working fine');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT ${PORT}`);
+});
